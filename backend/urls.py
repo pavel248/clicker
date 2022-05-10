40a6c1d9c4b9b672da00078688fe89b5a47d063f
pavelpetrovskij <pavel.petrovskiy.02@mail.ru>
2022-05-10 21:15:22 +0500

    3.3 added DRF functional

--- backend/urls.py
+++ backend/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('login/', views.user_login, name='login')
+    path('register/', views.Register.as_view(), name='register'),
+    path('login/', views.Login.as_view(), name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('', views.index, name='index'),
 ]
--- backend/views.py
+++ backend/views.py
@@ -2,37 +2,52 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from .forms import UserForm
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
-def register(request):
-    if request.method == 'POST':
+class Register(APIView):
+    def get(self, request):
+        form = UserForm()
+
+        return render(request, 'register.html', {'form': form})
+
+    def post(self, request):
         form = UserForm(request.POST)
+
         if form.is_valid():
             user = form.save()
             login(request, user)
+
             return redirect('index')
 
         return render(request, 'register.html', {'form': form})
 
-    form = UserForm()
-    return render(request, 'register.html', {'form': form})
 
+class Login(APIView):
+    def get(self, request):
+        form = UserForm()
 
-def user_login(request):
-    form = UserForm()
+        return render(request, 'login.html', {'form': form})
 
-    if request.method == 'POST':
-        user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
-        if user:
+    def post(self, request):
+        form = UserForm()
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
             login(request, user)
+
             return redirect('index')
 
         return render(request, 'login.html', {'form': form, 'invalid': True})
-
-    return render(request, 'login.html', {'form': form})
-
 
 @login_required
 def user_logout(request):
     logout(request)
     return redirect('login')
+
+def index(request):
+    return render(request, 'index.html', {})
